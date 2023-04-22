f02423b3095c463c352eed70f16cd1edd19e55f9
Angel S <santoyox714@gmail.com>
2023-04-22 06:23:23 -0700

    update
    fix nav
    fix form entry

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,15 +1,12 @@
-<!-- SearchBar.svelte -->
-<script>
-  import "../app.css";
-    
-</script>
-
-<nav class="flex items-center justify-between flex-wrap bg-white p-0">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
-    <a href="#" class="flex items-center">
-        <img src="https://mcnaughtonaviation.com/wp-content/uploads/2021/06/cropped-favicon.png" class="fill-current text-red-600 h-11 w-11" alt="Tripfinder Logo" />
-        <span class="fill-current text-black self-center text-2xl font-semibold whitespace-nowrap">Tripfinder</span>
-    </a>
-    
-    </div>
-</nav>
+<script lang="ts">
+  let src = '/plane.png'
+  
+  
+  </script>
+  
+        
+        <div class="pb-12 px-10 pt-4 text-right bg-slate-700 text-white"> 
+            <div class= "float-left pl-4 pb-0 font-bold text-xl"> <a href="/" class="button">Trip Finder</a>  </div>
+            <img class= "float-left pl-2" {src} width=40 alt="img" />
+           
+            </div>
