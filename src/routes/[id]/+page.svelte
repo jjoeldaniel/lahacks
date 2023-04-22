5043ac94542a01b1f4ac7f1a192ed54eb0bf999d
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-22 02:05:30 -0700

    translates id to city name

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,6 +1,32 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+  /** @type {import('./$types').PageData} */
+  export let data;
+
+  import Papa from "papaparse";
+  import { onMount } from "svelte";
+
+  let hashmap = {};
+
+  // Load the CSV file using fetch in onMount
+  onMount(async () => {
+    const response = await fetch("./uscities.csv");
+    const csv = await response.text();
+
+    Papa.parse(csv, {
+      header: true,
+      dynamicTyping: true,
+      skipEmptyLines: true,
+      complete: (results) => {
+        results.data.forEach((row) => {
+          const id = row.ID;
+          const city = row.city;
+
+          hashmap[id] = city;
+        });
+      },
+    });
+  });
 </script>
 
 <h1>{data.id}</h1>
+<p>{hashmap[data.id]}</p>
