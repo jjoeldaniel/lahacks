14904d09e97b80f8437db70cf3dc35ca58a428da
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-22 09:01:59 -0700

    changed color palette

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,17 +2,17 @@
   import "../app.css";
 </script>
 
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+<footer class="rounded-lg shadow m-4 bg-blue-600">
   <div
     class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"
   >
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+    <span class="text-sm text-white sm:text-center"
       >© 2023 <a href="https://flowbite.com/" class="hover:underline"
         >Tripfinder™</a
       >. All Rights Reserved.
     </span>
     <ul
-      class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+      class="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0"
     >
       <li>
         <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
