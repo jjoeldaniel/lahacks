2232817e24dde84b3dd802683f7f478265220bf8
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-04-22 14:30:52 -0700

    Updated Screen sizes
    Might need to change later based on what we put in the about section

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -3,8 +3,8 @@
   import "../app.css"
 </script>
 
-<div class="flex items-center pl-20 pt-10 text-right text-white">
-  <div class="grid grid-cols-2 gap-2 font-bold text-xl">
+<div class="flex items-center pl-20 pt-10 text-right text-white sm:justify-center">
+    <div class="grid grid-cols-2 gap-2 font-bold text-xl">
     <a href="/" class="col-span-1 button text-3xl">Trip Finder</a>
     <img class="col-start-2 bg-transparent" {src} width="50" alt="img" />
   </div>
