b4b092459dc3e156c5b761ab9ec8d74483dbb1b3
Angel S <santoyox714@gmail.com>
2023-04-22 00:19:09 -0700

    add search

---
+++ src/components/Search.svelte
@@ -0,0 +1,16 @@
+<!-- SearchBar.svelte -->
+<script>
+    export let onSearch;
+    let searchTerm = "";
+    
+    const handleSearch = () => {
+      onSearch(searchTerm);
+    };
+  </script>
+  
+  
+  <div>
+    <input type="text" bind:value={searchTerm} />
+    <button on:click={handleSearch}>Search</button>
+  </div>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- SearchPage.svelte -->
+<script>
+    import Search from "../components/Search.svelte"; 
+    import { goto } from '$app/navigation';
+    
+    
+             export let searchTerm
+    
+           export const handleSearch = (/** @type {any} */ searchTerm) => {
+                goto(`/${searchTerm}`);
+            };
+    
+            
+          </script>
+          
+          <h1>Search</h1>
+          
+          <Search onSearch={handleSearch} />
+          
