e86f0229386ae5e50d99d4e17e16fac9d843499a
vivbunii <vivicao@csu.fullerton.edu>
2023-04-22 11:33:42 -0700

    Create +page.svelte

---
+++ src/routes/(site)/privacy/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    
+</script>
+
+<p class="mb-3 text-lg text-gray-500 md:text-xl dark:text-gray-400">Privacy Policy</p>
+
+This privacy policy sets out how this application handles and uses any personal information collected from users of the Website.
+
+Scope of this Policy: This Policy applies solely to the Website and not to any third-party websites or services that may be linked to the Website.
+
+Data Collection and Use: The Website collects only emails of authorized users and does not store passwords, credentials or any other private information on authoirzed users. This website does not use cookies, gather IP addresses, or collect any other personal information.
+
+Changes to this Policy: The Website may update this Policy from time to time and will post any changes on this page.
+
+Conclusion: The Website is committed to protecting the privacy of its users and ensuring that their personal information is secure.
