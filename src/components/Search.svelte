1a058615108b5bfc34a51ef8709d570208644f7c
Angel S <santoyox714@gmail.com>
2023-04-22 03:57:11 -0700

    update
    Co-Authored-By: UNKNOWNtxn <80913719+UNKNOWNtxn@users.noreply.github.com>
    Co-Authored-By: vivbunii <112128164+vivbunii@users.noreply.github.com>

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -10,10 +10,9 @@
   };
 </script>
 
-<main class="h-screen w-screen overflow-hidden flex flex-col items-center justify-center bg-gray-800">
-  <div class="flex justify-center"></div>
-    <div class="grid grid-cols-6 gap-4 w-3/4 place-items-center">
-      <input class="bg-gray-600 col-span-5 px-4 py-2 w-full rounded-lg text-white" type="text" bind:value={searchTerm} />
-      <button class="col-start-6 px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 rounded-lg" on:click={handleSearch}>Search</button>
-    </div> 
-</main>
+<form class ="h-screen w-full flex items-center justify-center bg-slate-800"on:submit|preventDefault={() => handleSearch()}>
+  <div class="grid grid-cols-6 row-start-5 gap-4 w-3/4 place-items-center">
+    <input class="bg-gray-600 col-span-5  px-4 py-2 w-full rounded-lg text-white" type="text" bind:value={searchTerm} />
+    <button class="col-start-6 px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 rounded-lg">  Search </button>
+  </div>   
+</form>
