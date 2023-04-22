f294c19ec1791816b62235b8c134f4fa29c1d131
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-22 09:34:24 -0700

    added openai basic

--- src/routes/location/[id]/+page.server.js
+++ src/routes/location/[id]/+page.server.js
@@ -1,3 +1,11 @@
+const { Configuration, OpenAIApi } = require("openai");
+import { API_KEY } from '$env/static/private';
+
+const configuration = new Configuration({
+    apiKey: API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
     return {
