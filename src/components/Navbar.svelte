6032715f6911ec49c9c55c64c974643cbac4a2ff
Reece Bailey <80913719+UNKNOWNtxn@users.noreply.github.com>
2023-04-22 15:36:46 -0700

    Updated

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
   let src = "/la hacks logo.png";
-  import "../app.css"
+  import "../app.css";
 </script>
 
-
-<img src = "/la hacks logo.png" alt="venture co logo"> 
-
-<style>
-img {
-  padding: 10px;
-  width: 200px;
-  height: auto;
-}
-
-</style>
+<div class="flex items-center pl-20 text-right text-white">
+  <div class="grid grid-cols-2 gap-2">
+    <a href="/" class="mt-3">
+      <img src="/la hacks logo.png" width="80px" alt="venture co logo" />
+    </a>
+  </div>
+</div>
 
