5a1db6d8b99b7fa8e45da2d92dad45db0f854618
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-22 00:31:52 -0700

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,13 @@
 <!-- SearchPage.svelte -->
 <script>
-    import Search from "../components/Search.svelte"; 
-    import { goto } from '$app/navigation';
-    
-    
-             export let searchTerm
-    
-           export const handleSearch = (/** @type {any} */ searchTerm) => {
-                goto(`/${searchTerm}`);
-            };
-    
-            
-          </script>
-          
-          <h1>Search</h1>
-          
-          <Search onSearch={handleSearch} />
-          
+    import Search from "../components/Search.svelte";
+    import { goto } from "$app/navigation";
+
+    export const handleSearch = (/** @type {any} */ searchTerm) => {
+        goto(`/${searchTerm}`);
+    };
+</script>
+
+<h1>Search</h1>
+
+<Search onSearch={handleSearch} />
