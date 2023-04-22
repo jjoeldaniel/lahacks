7fbb1b0e4f8aa451eebbed0e2fec076e4aca0722
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-22 09:27:11 -0700

    fixed conflict issues

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -86,8 +86,23 @@
 >
   <div class="relative w-3/4">
     <div>
-      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+      <div
+        class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+      >
+        <svg
+          aria-hidden="true"
+          class="w-5 h-5 text-gray-500 dark:text-gray-400"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          /></svg
+        >
       </div>
       <input
         id="search-input"
@@ -99,45 +114,47 @@
       />
       <div class="relative">
         <button
-        class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Search
-      </button>
+          class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Search
+        </button>
       </div>
     </div>
 
     <!-- show the list of matches below the search bar -->
     <div class="absolute w-full">
       {#if filteredLocations.length > 0}
-      <div class="bg-gray-300 w-full rounded-lg col-span-5">
-        {#each filteredLocations as location, i}
-          <!-- on click, set the value of the search bar to the location and close the list -->
+        <div class="bg-gray-300 w-full rounded-lg col-span-5">
+          {#each filteredLocations as location, i}
+            <!-- on click, set the value of the search bar to the location and close the list -->
 
-        <!-- if i === hiLiteIndex, make bold -->
-        {#if i === hiLiteIndex}
-          <div
-            class="p-2 cursor-pointer bg-blue-500"
-            style="font-weight: bold; color: white;"
-            on:click={() => {
-              setInputVal(location);
-            }}
-          >
-            {location}
-          </div>
-        {/if}
-        {#if i != hiLiteIndex}
-          <div
-            class="p-2 cursor-pointer"
-            on:submit={() => {
-              goto(location);
-            }}
-            on:click={() => {
-              setInputVal(location);
-            }}
-          >
-            {location}
-          </div>{/if}
-      {/each}
+            <!-- if i === hiLiteIndex, make bold -->
+            {#if i === hiLiteIndex}
+              <div
+                class="p-2 cursor-pointer bg-blue-500"
+                style="font-weight: bold; color: white;"
+                on:click={() => {
+                  setInputVal(location);
+                }}
+              >
+                {location}
+              </div>
+            {/if}
+            {#if i != hiLiteIndex}
+              <div
+                class="p-2 cursor-pointer"
+                on:submit={() => {
+                  goto(location);
+                }}
+                on:click={() => {
+                  setInputVal(location);
+                }}
+              >
+                {location}
+              </div>{/if}
+          {/each}
+        </div>
+      {/if}
     </div>
   </div>
 </form>
